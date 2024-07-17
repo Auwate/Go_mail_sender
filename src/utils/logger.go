b8ce5a7c7e6e2b745b01518b3643fcdab3b03a92
Austin Uwate <auwate1@gmail.com>
2024-07-17 10:31:54 -0400

    Commit: Fix #28
    ## Description:
    - Adding logging

--- src/utils/logger.go
+++ src/utils/logger.go
@@ -9,6 +9,7 @@ import (
 func LoggerSetup(dirPath string) error {
 
 	_, err := os.Stat(dirPath)
+
 	if os.IsNotExist(err) {
 		if err := os.MkdirAll(dirPath, 0755); err != nil {
 			return err
@@ -16,15 +17,15 @@ func LoggerSetup(dirPath string) error {
 	} else if err != nil {
 		return err
 	}
-
+	log.Println("Log 1")
 	file, err := os.OpenFile(filepath.Join(dirPath, "log.txt"), os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
 
 	if err != nil {
 		return err
 	}
-
+	log.Println("Log 2")
 	log.SetOutput(file)
-
+	log.Println("Log 3")
 	return nil
 
 }
